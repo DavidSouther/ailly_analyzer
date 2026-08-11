dd160bd3ee6bab9ba24977192a461aa438187511
David Souther <davidsouther+github@gmail.com>
2026-08-11 12:48:23 -0400

    fix: specify Tauri runtime for Rust Analyzer
    Co-Authored-By: Ailly <developer@ailly.dev>

--- shell/src/lib.rs
+++ shell/src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    tauri::Builder::<tauri::Wry>::default()
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![app_ready])
         .run(tauri::generate_context!())
